4b31d42fa5f6f8b09f91ce3803a52a4d1aa4066a
AndresCRamos <andrescramosr@gmail.com>
2023-10-17 21:46:33 -0500

    Tested single event

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # pylint: disable=E1101
-
-from pkg.calendar import createCalendar, createEvent
+from pkg.calendar.calendar import create_calendar, create_event
 from pkg.unir.read import read_events
 
 def main():
     events = read_events()
+    calendar = create_calendar()
+    event_test = events[0]
+    create_event(calendar=calendar, **event_test)
 
 if __name__ == '__main__':
     main()
