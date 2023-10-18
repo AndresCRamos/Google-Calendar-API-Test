d07cfe07b331e2aa3097ff21e8a3e6ca1f513173
AndresCRamos <andrescramosr@gmail.com>
2023-10-17 20:39:37 -0500

    Implemented read and formatting for events
    This formats the JSON data to the Calendar API firm

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
-# pylint: disable=no-member
-from pkg.calendar import createCalendar, createEvent, logIn, createCalendarService
+# pylint: disable=E1101
+
+from pkg.calendar import createCalendar, createEvent
+from pkg.unir.read import read_events
 
 def main():
-    credentials = logIn()
-    cal_service = createCalendarService(credentials)
-    new_calendar_response = createCalendar(cal_service)
-    createEvent(
-        cal_service=cal_service,
-        calendar=new_calendar_response,
-        init_date="2023-10-17T17:00:00",
-        end_date="2023-10-17T17:30:00",
-        summary="Test Event",
-        description="Custom description"
-    )
-
+    events = read_events()
 
 if __name__ == '__main__':
     main()
---
+++ pkg/unir/read.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+from json import loads
+from datetime import datetime
+
+__DATA_PATH = Path("data.json").absolute()
+
+def __read_events_from_file()->list[dict]:
+    if __DATA_PATH.exists():
+        with open(file=__DATA_PATH, mode="r", encoding="utf-8") as f:
+            return loads(f.read())
+    else:
+        print("NO data to read")
+
+def __build_summary(**kwargs):
+    description = ""
+    theme_name = kwargs.get("Nombre_tema")
+    week = kwargs.get("Semana")
+    subthemes = kwargs.get("Subtemas")
+    activities = kwargs.get("Actividades")
+    if theme_name and kwargs.get("Tema")!= "":
+        description += f"{theme_name}\n"
+    if week:
+        description += f"{week}\n"
+    if subthemes:
+        description +="Subtemas:\n"
+        for subtheme in subthemes:
+            description += f"{subtheme}\n"
+    if activities:
+        description += f"Actividades: {activities}"
+    return description
+
+def __format_events(events:list[dict]):
+    formatted_events = []
+    for event in events:
+        summary = event.get("Tema") if event.get("Tema") != "" else event.get("Nombre_tema")
+        init_date = datetime.strptime(event.get("Fecha_inicio"), "%d/%m/%Y").date().strftime("%d-%m-%Y")
+        end_date = datetime.strptime(event.get("Fecha_inicio"), "%d/%m/%Y").date().strftime("%d-%m-%Y")
+        formatted_events.append({
+            "summary":  summary,
+            "init_date": init_date,
+            "end_date": end_date,
+            "description": __build_summary(**event)
+        })
+    return formatted_events
+
+def read_events():
+    unformatted_events = __read_events_from_file()
+    return  __format_events(unformatted_events)
